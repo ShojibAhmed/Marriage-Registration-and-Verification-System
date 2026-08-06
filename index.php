a8880ba4078e878d7b4572aaf812e935bc192b1f
MD SHOJIB AHMED <mdshojibahmed9706@gmail.com>
2026-08-06 22:18:27 +0600

    Initial project setup

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Marriage Registration and Verification Sys+te++m+</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet">
+
+</head>
+<body>
+
+
+<div class="container mt-5 text-center">
+
+    <h1 class="text-primary">
+        Marriage Registration and Verification System
+    </h1>
+
+    <p class="lead">
+        Final Year Project
+    </p>
+
+    <hr>
+
+    <h3 class="text-success">
+        Project is Running Successfully
+    </h3>
+
+</div>
+
+<?php
+echo "<h2>PHP is Working Successfully.</h2>";
+?>
+
+
+
+
+
+
+
+
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
